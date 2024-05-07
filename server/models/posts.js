218cdfafe0e80b136349a06f938756f10f7a0f78
Zoltan Kovac <zoltan.kovac@outlook.com>
2024-05-07 19:58:33 +0200

    init

---
+++ server/models/posts.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+  const Posts = sequelize.define("Posts", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    postText: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+
+  // `sequelize.define` also returns the model
+  console.log(Posts === sequelize.models.Posts); // true
+
+  return Posts;
+};
